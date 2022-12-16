d91546de5a37b22f29bc57024cbeac40aa1f9a9a
h31263219 <hades.aons@gmail.com>
2022-12-17 04:40:30 +0800

    Avatar Editor
    Deeplink to avatar editor
    Night Skybox

---
+++ tests/AvatarEnvTest/Assets/MyOpenAvatarEditor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyOpenAvatarEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenAvatarEditor()
+    {
+        print("Launching Avatar Editor");
+        AvatarEditorDeeplink.LaunchAvatarEditor();
+    }
+}
