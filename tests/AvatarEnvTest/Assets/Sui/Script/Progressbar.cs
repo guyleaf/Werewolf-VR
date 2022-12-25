0ea7845f93236458576025bac74536abaf291c11
SuYiHao <suyihao1999@gmail.com>
2022-12-25 14:43:36 +0800

    Vote UI progress bar

---
+++ tests/AvatarEnvTest/Assets/Sui/Script/Progressbar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progressbar : MonoBehaviour
+{
+    [SerializeField] public float remainTime;
+    [SerializeField] private float totalTime;
+    [SerializeField] public string titleString="Remain Time";
+
+    [SerializeField] private TMPro.TextMeshProUGUI progressText;
+    [SerializeField] private GameObject progressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressText.text = titleString + ": " + remainTime.ToString() + "s";
+        var theBarRectTransform = progressBar.transform as RectTransform;
+        theBarRectTransform.sizeDelta = new Vector2(390 * remainTime / totalTime, theBarRectTransform.sizeDelta.y);
+    }
+}
